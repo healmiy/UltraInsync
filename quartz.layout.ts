49599ac3360d93a7046d5fb1f63c828869a25816
Hilmi <healmiy@aol.com>
2025-06-26 09:25:32 +0800

    added giscuss

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,11 +5,26 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'Healmiy/PKMxKB',
+      // from data-repo-id
+      repoId: 'R_kgDOPB9zmg',
+      // from data-category
+      category: 'Announcements',
+      // from data-category-id
+      categoryId: 'DIC_kwDOPB9zms4CsBaG',
+      reactionsEnabled: true,
+    }
+  }),
+  ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/healmiy/PKMxKB",
+      "Adsvise.LLC": "https://adsvise.llc",
+      "Taxless.MY": "https://taxless.my",
     },
   }),
 }
